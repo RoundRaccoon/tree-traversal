e159b05ae5c875a57945942d9cef849d232e4db3
RoundRaccoon <ddaninn01@gmail.com>
2023-05-03 15:44:55 +0300

    task123

--- tree.py
+++ tree.py
@@ -1,5 +1,5 @@
 from node import Node
-
+import unittest
 
 class Tree:
     """ Tree class for binary tree """
@@ -62,27 +62,92 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
-        # TODO 1
+        """ Method for deleting a tree 
+        
+        Args:
+            None
+
+        Returns:
+            None
+        
+        """
         self.root = None
 
     def printTree(self):
-        # TODO 1
+        """ Method for printing a tree (inorder traversal)
+        
+        Args:
+            None
+
+        Returns:
+            None
+        
+        """
         if self.root is not None:
             self._printInorderTree(self.root)
 
     def _printInorderTree(self, node):
-        # TODO 1
+        """ Method for printing a tree's inorder traversal
+        
+        Args:
+            none (Node): current Node
+
+        Returns:
+            None
+        """
         if node is not None:
             self._printInorderTree(node.left)
             print(str(node.data) + ' ')
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO 2
-        pass
+        """ Method for printing a tree's preorder traversal
+        
+        Args:
+            none (Node): current Node
+
+        Returns:
+            None
+        """
+        if node is not None:
+            print(str(node.data) + ' ')
+            self._printInorderTree(node.left)
+            self._printInorderTree(node.right)
 
     def _printPostorderTree(self, node):
-        # TODO 2
-        pass
+        """ Method for printing a tree's postorder traversal
+        
+        Args:
+            none (Node): current Node
+
+        Returns:
+            None
+        """
+        if node is not None:
+            self._printInorderTree(node.left)
+            self._printInorderTree(node.right)
+            print(str(node.data) + ' ')
 
 
+class TestTreeFind(unittest.TestCase):
+    def test_find_found(self):
+        root = Tree()
+        root.add(1)
+        root.add(2)
+        root.add(3)
+        root.add(4)
+        root.add(5)
+        root.add(6)
+        root.add(7)
+        assert root.find(4) is not None
+    
+    def test_find_not_found(self):
+        root = Tree()
+        root.add(1)
+        root.add(2)
+        root.add(3)
+        root.add(4)
+        root.add(5)
+        root.add(6)
+        root.add(7)
+        assert root.find(8) is None
